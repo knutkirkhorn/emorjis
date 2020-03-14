2fcb76000f9853f7dcf4b29c5aed554191e687be
Knut Kirkhorn <knutkirk@hotmail.com>
2020-03-14 14:48:22 +0100

    Update dependency

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import test from 'ava';
-import Util from './src/js/Util';
+const test = require('ava');
+const Util = require('./src/js/Util');
 
 const arrayToCopy = [];
 for (let i = 0; i < 5; i++) {
